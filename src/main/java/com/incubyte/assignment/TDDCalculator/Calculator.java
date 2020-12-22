91343174f69511bdb41f4da2974f885ab051038d
SiyaAgarwal98 <siyaagarwal98@gmail.com>
2020-12-22 21:39:06 +0530

    Step 5: negatives not allowed

--- src/main/java/com/incubyte/assignment/TDDCalculator/Calculator.java
+++ src/main/java/com/incubyte/assignment/TDDCalculator/Calculator.java
@@ -1,12 +1,14 @@
 package com.incubyte.assignment.TDDCalculator;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 
 public class Calculator {
 
-	public static int add(String numbers) {
+	public static int add(String numbers) throws Exception {
 		int sum = 0;
 		if (!StringUtils.isEmpty(numbers)) {
 			String delimiter=",|\\n";
@@ -16,6 +18,17 @@ public class Calculator {
 				delimiter=parts[0].substring(2);
 			}
 			int[] intArray = Arrays.stream(numbers.split(delimiter)).mapToInt(Integer::parseInt).toArray();
+			
+			List<Integer> negatives = new ArrayList<>();
+			Arrays.stream(intArray).forEach(num -> {
+				if (num < 0)
+					negatives.add(num);
+			});
+			
+			if(!negatives.isEmpty()) {
+				throw new Exception("negatives not allowed" + negatives.toString());
+			}
+			
 			for (int i : intArray) {
 				sum = sum + i;
 			}
--- src/test/java/com/incubyte/assignment/TDDCalculator/CalculatorTests.java
+++ src/test/java/com/incubyte/assignment/TDDCalculator/CalculatorTests.java
@@ -4,43 +4,46 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import com.incubyte.assignment.TDDCalculator.Calculator;
-
 public class CalculatorTests {
 
 	@Test
-	public void addEmptyString() {
+	public void addEmptyString() throws Exception {
 		int sum = Calculator.add("");
 		assertEquals(0, sum);
 	}
 
 	@Test
-	public void addOneNum() {
+	public void addOneNum() throws Exception {
 		int sum = Calculator.add("1");
 		assertEquals(1, sum);
 	}
 
 	@Test
-	public void addTwoNums() {
+	public void addTwoNums() throws Exception {
 		int sum = Calculator.add("1,2");
 		assertEquals(3, sum);
 	}
 
 	@Test
-	public void addNNums() {
+	public void addNNums() throws Exception {
 		int sum = Calculator.add("1,2,4,6");
 		assertEquals(13, sum);
 	}
-	
+
 	@Test
-	public void addAndHandleNewLines() {
+	public void addAndHandleNewLines() throws Exception {
 		int sum = Calculator.add("1\n2,3");
 		assertEquals(6, sum);
 	}
-	
+
 	@Test
-	public void changeDelimiterAndAdd() {
+	public void changeDelimiterAndAdd() throws Exception {
 		int sum = Calculator.add("//;\n1;2");
 		assertEquals(3, sum);
+	}
+
+	@Test(expected = Exception.class)
+	public void negativesNotAllowed() throws Exception {
+		Calculator.add("//;\n-1;2");
 	}
 }
