4c60b62b31048f4e4d107e6c6eece272fddd06ec
SiyaAgarwal98 <siyaagarwal98@gmail.com>
2020-12-23 13:16:27 +0530

    Final refactoring done

--- src/main/java/com/incubyte/assignment/TDDCalculator/Calculator.java
+++ src/main/java/com/incubyte/assignment/TDDCalculator/Calculator.java
@@ -4,43 +4,106 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
 
 public class Calculator {
 
-	public static int add(String numbers) throws Exception {
+	private static String NEWLINE_CHAR = "\\n";
+	private static String DEFAULT_DELIMITER = ",|\\n";
+	private static String NEGATIVES_NOT_ALLOWED = "negatives not allowed";
+	private static String EMPTY_STRING = "";
+	private static String SQUARE_BRACKETS = "\\[|\\]";
+	private static String CHANGE_DELIMITER_IDENTIFIER = "//";
+	private static String OR_OPERATOR = "|";
+
+	public static int add(String input) throws Exception {
 		int sum = 0;
-		if (!StringUtils.isEmpty(numbers)) {
-			String delimiter=",|\\n";
-			if (numbers.startsWith("//")) {
-				String[] parts = numbers.split("\\n", 2);
-				numbers = parts[1];
-				ArrayList<String> delimiters = new ArrayList<>(
-						Arrays.stream(parts[0].substring(2).split("\\[|\\]")).collect(Collectors.toList()));
-				delimiters.removeAll(Arrays.asList("", null));
-				for (String d : delimiters) {
-					delimiter = delimiter + "|" + Pattern.quote(d);
-				}
+		String strOfNums = null;
+		String delimiters = null;
+
+		if (!StringUtils.isEmpty(input)) {
+			int[] arrOfNums;
+			if (isDelimiterChanged(input)) {
+
+				String[] parts = input.split(NEWLINE_CHAR, 2);
+
+				strOfNums = parts[1];
+				delimiters = getDelimiters(parts[0]);
+
+			} else {
+
+				strOfNums = input;
+				delimiters = DEFAULT_DELIMITER;
 			}
-			int[] intArray = Arrays.stream(numbers.split(delimiter)).mapToInt(Integer::parseInt).toArray();
-			
-			List<Integer> negatives = new ArrayList<>();
-			Arrays.stream(intArray).forEach(num -> {
-				if (num < 0)
-					negatives.add(num);
-			});
-			
-			if(!negatives.isEmpty()) {
-				throw new Exception("negatives not allowed" + negatives.toString());
-			}
-			
-			for (int i : intArray) {
-				sum = i > 1000 ? sum : sum + i;
-			}
+
+			arrOfNums = getArrOfNums(strOfNums, delimiters);
+
+			disallowNegatives(arrOfNums);
+
+			sum = sumOfNums(arrOfNums);
+
 		}
 		return sum;
 	}
 
+	private static int[] getArrOfNums(String strOfNums, String delimiter) {
+		String[] strArrOfNums = strOfNums.split(delimiter);
+		int[] intArrOfNums = Arrays.stream(strArrOfNums).mapToInt(Integer::parseInt).toArray();
+
+		return intArrOfNums;
+	}
+
+	private static void disallowNegatives(int[] arrOfNums) throws Exception {
+		List<Integer> negatives = getListOfNegatives(arrOfNums);
+
+		if (!negatives.isEmpty()) {
+			throw new Exception(NEGATIVES_NOT_ALLOWED + negatives.toString());
+		}
+	}
+
+	private static List<Integer> getListOfNegatives(int[] arrOfNums) {
+		List<Integer> negatives = new ArrayList<>();
+		Arrays.stream(arrOfNums).forEach(num -> {
+			if (num < 0)
+				negatives.add(num);
+		});
+
+		return negatives;
+	}
+
+	private static String getDelimiters(String strOfDelimiters) {
+		String delimiters = null;
+		List<String> listOfDelimiters = getListOfDelimiters(strOfDelimiters);
+
+		for (String d : listOfDelimiters) {
+			delimiters = StringUtils.isEmpty(delimiters) ? Pattern.quote(d)
+					: String.join(OR_OPERATOR, delimiters, Pattern.quote(d));
+		}
+
+		return delimiters;
+	}
+
+	private static List<String> getListOfDelimiters(String strOfDelimiters) {
+		ArrayList<String> arrayListOfDelimiters = new ArrayList<>(
+				Arrays.asList(strOfDelimiters.substring(2).split(SQUARE_BRACKETS)));
+		arrayListOfDelimiters.removeAll(Arrays.asList(EMPTY_STRING, null));
+
+		return arrayListOfDelimiters;
+	}
+
+	private static boolean isDelimiterChanged(String input) {
+		return input.startsWith(CHANGE_DELIMITER_IDENTIFIER);
+	}
+
+	private static int sumOfNums(int[] intArray) {
+
+		int sum = 0;
+
+		for (int i : intArray) {
+			sum = i > 1000 ? sum : sum + i;
+		}
+
+		return sum;
+	}
 }
